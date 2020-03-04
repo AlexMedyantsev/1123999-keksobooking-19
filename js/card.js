b01829a6c1fb56c581f5d40d1877fcb6c1225d46
AlexMedyantsev <medyantsev1978@icloud.com>
2020-03-04 12:57:53 +0300

    Поменял название setPinHandlers на setHandlers. Теперь оно соответсвует критерию Д3
    Исправил синтаксис
    
    Убрал trailing spaces
    
    Поменял название setPinHandlers на setHandlers.

--- js/card.js
+++ js/card.js
@@ -120,39 +120,39 @@
 
   window.constants.MAIN_MAP_PIN.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
-    
+
     var startCoords = {
       x: evt.clientX,
       y: evt.clientY
-    }
+    };
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-  
+
       var shift = {
         x: startCoords.x - moveEvt.clientX,
         y: startCoords.y - moveEvt.clientY
       };
-  
+
       startCoords = {
         x: moveEvt.clientX,
         y: moveEvt.clientY
       };
-  
+
       window.constants.MAIN_MAP_PIN.style.top = (window.constants.MAIN_MAP_PIN.offsetTop - shift.y) + 'px';
       window.constants.MAIN_MAP_PIN.style.left = (window.constants.MAIN_MAP_PIN.offsetLeft - shift.x) + 'px';
     };
-  
+
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
-  
+
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     };
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
-  })
+  });
   window.card = {
     open: openCard,
   };
