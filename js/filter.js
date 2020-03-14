0685a40e8079065bc5fd2d225d4f63cca8ca9b2e
AlexMedyantsev <medyantsev1978@icloud.com>
2020-03-14 13:19:53 +0300

    Фильтр

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+
+  var filterPins = function (pin) {
+    houseTypeCheck(pin);
+    // priceCheck();
+    // roomsAmountCheck(pin);
+  };
+
   var houseTypeCheck = function (pin) {
     if (window.constants.MAP_FILTER_HOUSE_TYPE.value === 'any') {
       return pin;
@@ -9,9 +16,25 @@
   };
 
   var updatePins = function () {
-    var filteredPins = window.data.get().filter(houseTypeCheck);
+    var filteredPins = window.data.get().filter(filterPins);
     window.pin.render(filteredPins);
   };
+
+  // var priceCheck = function () {
+  //   switch (window.constants.MAP_FILTER_PRICE.value) {
+  //      case any:
+  //        return pin.offer.price === window.constants.MAP_FILTER_PRICE.value;
+  //        break;
+  //   }
+  // };
+
+  // var roomsAmountCheck = function (pin) {
+  //   if (window.constants.MAP_FILTER_ROOMS.value === 'any') {
+  //     return pin;
+  //   }
+  //   return pin.offer.rooms === window.constants.MAP_FILTER_ROOMS.value;
+  // };
+
 
   window.filter = {
     updatePins: updatePins,
