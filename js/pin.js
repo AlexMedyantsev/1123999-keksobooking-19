a728f4de61a6298256d8c3d1c657a0a8f37374be
AlexMedyantsev <medyantsev1978@icloud.com>
2020-03-16 15:16:29 +0300

    Убрал удаленную функцию коллбек, и поставил на её месте пустую анонимную функцию

--- js/pin.js
+++ js/pin.js
@@ -108,7 +108,8 @@
 
   var onMainPinClick = function () {
     window.constants.MAIN_MAP_PIN.addEventListener('click', function handler() {
-      window.server.loadData(window.pin.onDataLoaded, window.pin.onDataLoadError);
+      window.server.loadData(window.pin.onDataLoaded, function (){
+      });
       window.constants.MAIN_MAP_PIN.removeEventListener('click', handler);
     });
   };
