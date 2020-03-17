c34705d826a99313b806f161d15f105545a86642
AlexMedyantsev <medyantsev1978@icloud.com>
2020-03-17 16:25:58 +0300

    Б13. Исправление
    Убрал неиспользующийся код

--- js/utils.js
+++ js/utils.js
@@ -1,32 +1,6 @@
 'use strict';
 
 (function () {
-  var getRandomInteger = function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  };
-
-  var getRandomStringFromArray = function (array) {
-    return array[getRandomInteger(0, array.length - 1)];
-  };
-
-  var shuffleArray = function (array) {
-    return array.sort(function () {
-      return 0.5 - Math.random();
-    });
-  };
-
-  var generateArrayOfStrings = function (array) {
-    var resultArrayLength = getRandomInteger(1, array.length);
-    var resultArray = [];
-
-    var shuffledArray = shuffleArray(array);
-
-    for (var i = 0; i < resultArrayLength; i++) {
-      resultArray[i] = shuffledArray[i];
-    }
-    return resultArray;
-  };
-
   var getElementLocation = function (element) {
     var locationCoordinates = {
       'location': {
@@ -71,10 +45,6 @@
   };
 
   window.utils = {
-    getRandomInteger: getRandomInteger,
-    getRandomStringFromArray: getRandomStringFromArray,
-    shuffleArray: shuffleArray,
-    generateArrayOfStrings: generateArrayOfStrings,
     getElementLocation: getElementLocation,
     setElementPosition: setElementPosition,
     getElementMiddleBottomPosition: getElementMiddleBottomPosition,
